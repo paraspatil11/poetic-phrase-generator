db4a8d710d5acda69848a2b75b5eb595ea4b934b
Paras Chetan Patil <119652582+paraspatil11@users.noreply.github.com>
2023-08-10 20:36:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 import { generateTextAndImage } from "./utils.js"
 
-// 1. Change the value of the variable to your name
-let name = "Guil Hernandez"
+let name = "Paras Patil"
 
-// 2. Change the value of the variable to your favorite activity
-let favoriteActivity = "kayaking"
+let favoriteActivity = "reading"
 
-// 3. Assign the favoritePlace variable your favorite place
-// I.e. city, mountain, pub, forrest, beach, Manhattan, etc.
-let favoritePlace = "florida keys"
+let favoritePlace = "library"
 
-// 4. Configure the AI by setting a temperature from 0 to 1
-// The higher temperature, the more random & experimental output
 let temperature = 1
-
-// Optional: delete "avatar.jpg" and add a photo of yourself
-// (remember to use "avatar.jpg" as the name of your photo)
 
 generateTextAndImage(name, favoriteActivity, favoritePlace, temperature)
